5c82f71aceb65b21eaea95181425ea56c4f7b631
cryosx <cyruswu.email@gmail.com>
2018-04-17 13:00:00 -1000

    Divide function

--- code.js
+++ code.js
@@ -1,11 +1,15 @@
 function add(x, y) {
-  return x + y;
+  return Number.parseFloat(x) + Number.parseFloat(y);
 }
 
 function subtract(x, y) {
   return Number.parseFloat(x) - Number.parseFloat(y);
 }
 
-function multiply(x, y){
+function multiply(x, y) {
   return Number.parseFloat(x) * Number.parseFloat(y);
+}
+
+function divide(x, y) {
+  return Number.parseFloat(x) / Number.parseFloat(y);
 }
